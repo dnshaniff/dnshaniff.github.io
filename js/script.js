c552e21d63c988b88a1cdf50561eaa21751c5d44
dnshaniff <98203337+dnshaniff@users.noreply.github.com>
2023-06-24 10:03:54 +0700

    update script.js for the new portfolio

--- js/script.js
+++ js/script.js
@@ -1,141 +1,87 @@
-// parallax
-// js Ketika halaman di-load
-$(window).on('load', function() {
-  setTimeout(function() {
-    $('.jintro').addClass('jMuncul');
-    $('.ptype').addClass('jMuncul');
-    $('.sosmed').addClass('jMuncul');
-    $('.cv').addClass('jMuncul');
-    $('.jimg').addClass('jMuncul');
-  }, 200);
-})
-  
-  
-  
-// jsScroll
-$(window).scroll(function() {
-  var wScroll = $(this).scrollTop();
-    
-// munculin langsung semua
-if( wScroll > $('.about').offset().top - 150 ) {
-  $('.about').addClass('aMuncul');
-}
-if( wScroll > $('.skills').offset().top - 150 ) {
-  $('.skills').addClass('sMuncul');
-}
-if( wScroll > $('.projects').offset().top - 150 ) {
-  $('.projects').addClass('pMuncul');
-}
-if( wScroll > $('.contact').offset().top - 100 ) {
-  $('.contact').addClass('cMuncul');
-}
-  
-// muncul satu-satu
-    if( wScroll > $('.projects').offset().top - 250 ) {
-      $('.projects .main-card').each(function(i) {
-        setTimeout(function() {
-          $('.projects .main-card').eq(i).addClass('muncul');
-        }, 350 * (i+1));
+// Scrollable
+window.addEventListener('scroll', function() {
+  var navbar = document.querySelector('.navbar');
+  var btnBackToTop = document.querySelector('#btn-back-to-top');
+  var scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+  var sections = document.querySelectorAll('section');
+
+  sections.forEach(function(section) {
+    var sectionTop = section.offsetTop;
+    var sectionHeight = section.clientHeight;
+
+    if (scrollPosition >= sectionTop - navbar.offsetHeight && scrollPosition < sectionTop + sectionHeight) {
+      var navItems = navbar.querySelectorAll('.nav-item');
+      navItems.forEach(function(navItem) {
+        navItem.classList.remove('active');
       });
+
+      var currentNavItem = navbar.querySelector('a[href="#' + section.getAttribute('id') + '"]').closest('.nav-item');
+      if (currentNavItem) {
+        currentNavItem.classList.add('active');
+      }
     }
-  
   });
-  
-  // get button
-  let mybutton = document.getElementById("btn-back-to-top");
-  
-  // navbar change color
-  const navbar = document.querySelector('.navbar');
-  
-  window.onscroll = function(){
-    scrollFunction();
-    function scrollFunction() {
-    // button to top
-      if (
-        document.body.scrollTop > 20 ||
-        document.documentElement.scrollTop > 20
-      ) {
-        mybutton.style.display = "block";
-      } else {
-        mybutton.style.display = "none";
-      }
-    } 
-  
-    // navbar change color
-    var top = window.scrollY;
-    console.log(top);
-    if(top >= 140) {
-      navbar.classList.add('change')
-    }else{
-      navbar.classList.remove('change');
-    }
-  
-    // menu navbar active ketika scroll
-    var scrollTop = $(document).scrollTop();
-    var anchors = $('body').find('section');
-      for (var i = 0; i < anchors.length; i++){
-        if (scrollTop > $(anchors[i]).offset().top - 190 && scrollTop < $(anchors[i]).offset().top + $(anchors[i]).height() - 200) {
-           $('nav ul li a[href="#' + $(anchors[i]).attr('id') + '"]').addClass('active');
-          }else{
-            $('nav ul li a[href="#' + $(anchors[i]).attr('id') + '"]').removeClass('active');
-          }
-      }
+
+  if (scrollPosition > 760) {
+    navbar.classList.add('scrolled');
+    btnBackToTop.style.display = 'block';
+  } else {
+    navbar.classList.remove('scrolled');
+    btnBackToTop.style.display = 'none';
   }
-  
-  // klik button akan kembali ke atas
-  mybutton.addEventListener("click", backToTop);
-  
-  function backToTop() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-  };
-  
-  // untuk merubah class active pada menu yang diklik
-  var btns = 
-  $(".navbar .navbar-nav .nav-link");
-    for (var i = 0; i < btns.length; i++) {
-      btns[i].addEventListener("click", function () {
-        var current = document
-          .getElementsByClassName("active");
-  
-        current[0].className = current[0]
-          .className.replace(" active", "");
-        this.className += " active";
-      });
-    }
+});
 
-// typing effect
-  const typedTextSpan=document.querySelector(".typed-text");
-  const cursorSpan=document.querySelector(".cursor");
-  const textArray=["Web Developer","Front-End Developer","Freshgraduate"];
-  const typingDelay=200;
-  const erasingDelay=100;
-  const newTextDelay=1500;
-    let textArrayIndex=0;
-    let charIndex=0;
-    function type() {
-      if(charIndex<textArray[textArrayIndex].length)  {
-        if(!cursorSpan.classList.contains("typing"))cursorSpan.classList.add("typing");
-          typedTextSpan.textContent+=textArray[textArrayIndex].charAt(charIndex);
-          charIndex++;
-          setTimeout(type,typingDelay);
+var btnBackToTop = document.querySelector('#btn-back-to-top');
+btnBackToTop.addEventListener('click', function() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
+
+// Typewriting
+const typedTextSpan=document.querySelector(".typed-text");
+const cursorSpan=document.querySelector(".cursor");
+const textArray=["Web Developer", "Web Developer - Back-End"];
+const typingDelay=50;
+const erasingDelay=100;
+const newTextDelay=1500;
+  let textArrayIndex=0;
+  let charIndex=0;
+  function type() {
+    if(charIndex<textArray[textArrayIndex].length)  {
+      if(!cursorSpan.classList.contains("typing"))cursorSpan.classList.add("typing");
+        typedTextSpan.textContent+=textArray[textArrayIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type,typingDelay);
+    }else{
+      cursorSpan.classList.remove("typing");setTimeout(erase,newTextDelay);
+    }
+  }
+
+  function erase()  {
+    if(charIndex>0) {
+      if(!cursorSpan.classList.contains("typing"))cursorSpan.classList.add("typing");
+        typedTextSpan.textContent=textArray[textArrayIndex].substring(0,charIndex-1);
+        charIndex--;
+        setTimeout(erase,erasingDelay);
       }else{
-        cursorSpan.classList.remove("typing");setTimeout(erase,newTextDelay);
+        cursorSpan.classList.remove("typing");
+        textArrayIndex++;
+        if(textArrayIndex>=textArray.length)textArrayIndex=0;
+        setTimeout(type,typingDelay+1100);
       }
     }
+  document.addEventListener("DOMContentLoaded",function(){if(textArray.length)setTimeout(type,newTextDelay+250);
+});
 
-    function erase()  {
-      if(charIndex>0) {
-        if(!cursorSpan.classList.contains("typing"))cursorSpan.classList.add("typing");
-          typedTextSpan.textContent=textArray[textArrayIndex].substring(0,charIndex-1);
-          charIndex--;
-          setTimeout(erase,erasingDelay);
-        }else{
-          cursorSpan.classList.remove("typing");
-          textArrayIndex++;
-          if(textArrayIndex>=textArray.length)textArrayIndex=0;
-          setTimeout(type,typingDelay+1100);
-        }
-      }
-    document.addEventListener("DOMContentLoaded",function(){if(textArray.length)setTimeout(type,newTextDelay+250);
-    });
+// Content-show
+document.addEventListener("DOMContentLoaded", function() {
+  const col5 = document.querySelector(".hero-left");
+  const col7 = document.querySelector(".hero-right");
+  const elevatorPitch = document.querySelector(".hero-bottom");
+
+  col5.style.opacity = "1";
+  col7.style.opacity = "1";
+  elevatorPitch.style.opacity = "1";
+});
